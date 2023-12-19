9f3f39a63f326dea18f9a1b5685f6ed423621f9e
Fojilun Hussain <fojilun.hussain@gmail.com>
2023-12-19 12:30:04 +0000

    determine if fingers on right hand are up

--- main.py
+++ main.py
@@ -20,8 +20,31 @@ while True:
     results = hands.process(image)
 
     if results.multi_hand_landmarks:
+        # hand_orientation = {
+        #     "left_palm_visible": True,
+        #     "right_palm_visible": True
+        # }
+
         for hand_landmarks in results.multi_hand_landmarks:
             finger_states = []
+
+            # Determine hand orientation
+
+            # if results.multi_hand_landmarks[0].landmark[0].x > results.multi_hand_landmarks[0].landmark[1].x:
+            #     hand_orientation["right_palm_visible"] = True
+
+
+            # Defining finger coordinates
+            
+            if results.multi_hand_landmarks[0].landmark[4].x > results.multi_hand_landmarks[0].landmark[3].x:
+                print("Thumb is up")
+                finger_states.append(1)
+
+            for tip_id in fingertip_ids:
+                if tip_id != 4 and results.multi_hand_landmarks[0].landmark[tip_id].y < results.multi_hand_landmarks[0].landmark[tip_id - 1].y and results.multi_hand_landmarks[0].landmark[tip_id].y < results.multi_hand_landmarks[0].landmark[tip_id - 2].y:
+                    print("Finger is up")
+                    finger_states.append(1)
+
 
             # Extract and draw landmarks on the image
 
@@ -39,11 +62,11 @@ while True:
 
     cv2.imshow("What's cookin', good lookin'?", frame)
 
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d_%H%M%S")
-    capture_name = f"capture_{timestamp}.png"
+    # now = datetime.now()
+    # timestamp = now.strftime("%Y%m%d_%H%M%S")
+    # capture_name = f"capture_{timestamp}.png"
 
-    cv2.imwrite(f"{image_capture_folder}{capture_name}", image)
+    # cv2.imwrite(f"{image_capture_folder}{capture_name}", image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
