498159ce515cb8b3fa0599a2c46a9c4affdb2964
Fojilun Hussain <fojilun.hussain@gmail.com>
2023-12-19 01:05:58 +0000

    distinguish between hand and finger parts, set fingertip colour

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ image_capture_folder = "captures/"
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+fingertip_ids = [4, 8, 12, 16, 20]
 
 cap = cv2.VideoCapture(0)
 
@@ -20,22 +21,28 @@ while True:
 
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-        # Draw hand landmarks on the image
-            for lm in hand_landmarks.landmark:
-                # Extract landmark positions (lm.x, lm.y, lm.z)
-                # Draw landmarks on the image
+            finger_states = []
+
+            # Extract and draw landmarks on the image
+
+            for id, hand_lm in enumerate(hand_landmarks.landmark):
                 h, w, c = frame.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+                cx, cy = int(hand_lm.x * w), int(hand_lm.y * h)
+
+                if id in fingertip_ids:
+                    cv2.circle(frame, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+                else:
+                    cv2.circle(frame, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+            finger_count = finger_states.count(1)
+            cv2.putText(frame, f"Finger count: {finger_count}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("What's cookin', good lookin'?", frame)
 
-    # append date and time to capture name
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d_%H%M%S")
     capture_name = f"capture_{timestamp}.png"
 
-    # save image with appended capture name
     cv2.imwrite(f"{image_capture_folder}{capture_name}", image)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
