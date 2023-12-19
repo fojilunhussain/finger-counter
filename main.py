bd5cef490510241a0bb5b036d571db06dd32ca0e
Fojilun Hussain <fojilun.hussain@gmail.com>
2023-12-19 19:40:21 +0000

    split code into different functions

--- main.py
+++ main.py
@@ -9,11 +9,37 @@ fingertip_ids = [4, 8, 12, 16, 20]
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    result, frame = cap.read()
-    if not result:
-        break
+def determine_finger_state(multi_hand_landmarks, label, finger_states):
+    if label == "Left":
+        if multi_hand_landmarks[4].x > multi_hand_landmarks[3].x:
+            print(f"{label} thumb is up")
+            finger_states.append(1)
+    if label == "Right":
+        if multi_hand_landmarks[4].x < multi_hand_landmarks[3].x:
+            print(f"{label} thumb is up")
+            finger_states.append(1)
 
+    for tip_id in fingertip_ids:
+        if tip_id != 4 and multi_hand_landmarks[tip_id].y < multi_hand_landmarks[tip_id - 1].y and multi_hand_landmarks[tip_id].y < multi_hand_landmarks[tip_id - 2].y:
+            print("Finger is up")
+            finger_states.append(1)
+
+def detect_handedness(finger_states, results):
+    for hand, handedness in enumerate(results.multi_handedness):
+        label = handedness.classification[0].label
+        score = handedness.classification[0].score
+
+        print(f"Hand {hand + 1}: {label} hand, Confidence: {score:.2f}")
+
+        determine_finger_state(results.multi_hand_landmarks[hand].landmark, label, finger_states)
+
+def draw_landmarks(frame, hand_landmarks):
+    mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+def count_fingers(finger_states):
+    return finger_states.count(1)
+
+def process_frame(frame):
     flipped_frame = cv2.flip(frame, 1) # TODO: Test with different webcams.
     image = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2RGB)
 
@@ -23,40 +49,28 @@ while True:
         for hand_landmarks in results.multi_hand_landmarks:
             finger_states = []
             
-            # Detecting handedness
+            detect_handedness(finger_states, results)
 
-            for hand, handedness in enumerate(results.multi_handedness):
-                label = handedness.classification[0].label
-                score = handedness.classification[0].score
+            draw_landmarks(flipped_frame, hand_landmarks)
 
-                print(f"Hand {hand + 1}: {label} hand, Confidence: {score:.2f}")
-
-                # Defining finger coordinates
-
-                if label == "Left":
-                    if results.multi_hand_landmarks[hand].landmark[4].x > results.multi_hand_landmarks[hand].landmark[3].x:
-                        print(f"{label} thumb is up")
-                        finger_states.append(1)
-                if label == "Right":
-                    if results.multi_hand_landmarks[hand].landmark[4].x < results.multi_hand_landmarks[hand].landmark[3].x:
-                        print(f"{label} thumb is up")
-                        finger_states.append(1)
-
-                for tip_id in fingertip_ids:
-                    if tip_id != 4 and results.multi_hand_landmarks[hand].landmark[tip_id].y < results.multi_hand_landmarks[hand].landmark[tip_id - 1].y and results.multi_hand_landmarks[hand].landmark[tip_id].y < results.multi_hand_landmarks[hand].landmark[tip_id - 2].y:
-                        print("Finger is up")
-                        finger_states.append(1)
-
-            # Extract and draw landmarks on the image
-            mp_drawing.draw_landmarks(flipped_frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-            finger_count = finger_states.count(1)
+            finger_count = count_fingers(finger_states)
             cv2.putText(flipped_frame, f"Finger count: {finger_count}", (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
     cv2.imshow("What's cookin', good lookin'?", flipped_frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def main():
+    while True:
+        result, frame = cap.read()
+        if not result:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+        process_frame(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
